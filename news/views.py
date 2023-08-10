1c727958701072d6376dc71947383ceda03ae316
beliyaka98 <the.akniyet.second@gmail.com>
2023-08-11 01:30:41 +0600

    added last task

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import New
+from .models import New, Comment
 
 def news(request):
     data_news = New.objects.all().order_by("-created_at")
@@ -8,6 +8,10 @@ def news(request):
 
 def detail(request, id):
     data_new = get_object_or_404(New, id=id)
+    if request.method == "POST":
+        comment_new = Comment(content=request.POST["content"], new=data_new)
+        comment_new.save()
+        return redirect("detail", id=data_new.id)
     return render(request, "news/detail.html", {"new": data_new})
 
 def create_new(request):
